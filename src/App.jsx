d65e1b90ed1075eba78233a6e1dc51587f45459f
alejoevilches <alejoevilches@gmail.com>
2023-09-14 13:31:52 -0300

    add spa structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+const NAVIGATION_EVENT="pushstate"
+
+function navigate(href){
+  window.history.pushState({}, "", href);
+  const navigationEvent=new Event (NAVIGATION_EVENT)
+  window.dispatchEvent(navigationEvent);
+}
 
 function HomePage(){
   return (
     <>
       <h1>Home</h1>
       <p>Esta es una pagina de ejemplo para crear nuestro React Router</p>
-      <a href="/about">Ir a Sobre Nosotros</a>
+      <button onClick={()=>{navigate("/about")}}>Ir a Sobre Nosotros</button>
     </>
   )
 }
@@ -15,13 +23,25 @@ function About(){
     <>
       <h1>About</h1>
       <p>Hola! Mi nombre es Alejo y estoy creando un clon de React Router</p>
-      <a href="/">Ir a la home page</a>
+      <button href="/" onClick={()=>{navigate("/")}}>Ir a la home page</button>
     </>
   )
 }
 
 function App() {
   const [currentPath, setCurrentPath]=useState(window.location.pathname);
+  useEffect(()=>{
+    const onLocationChange=()=>{
+      setCurrentPath(window.location.pathname);
+    }
+
+    window.addEventListener(NAVIGATION_EVENT, onLocationChange);
+    window.addEventListener("popstate", onLocationChange);
+
+    return ()=>{
+      window.removeEventListener(NAVIGATION_EVENT, onLocationChange);
+    }
+  }, [])
   return (
     <main>
       {currentPath == "/" && <HomePage/>}
