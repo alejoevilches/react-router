68600e2fb301e8a94ecbace7589a1c5bc53c6844
alejoevilches <alejoevilches@gmail.com>
2023-09-16 18:12:51 -0300

    fix test suite

--- router.test.jsx
+++ router.test.jsx
@@ -1,9 +1,32 @@
-import {describe, it, expect} from "vitest";
-import {render} from "@testing-library/react";
+import {describe, it, expect, beforeEach} from "vitest";
+import {render, screen} from "@testing-library/react";
 import {Router} from "./src/components/Router"
+import { cleanup } from "@testing-library/react";
 
 describe("Router", ()=>{
-    it("should work", ()=>{
+    beforeEach(()=>{
+        cleanup();
+    })
+    it("should work without problems", ()=>{
         render(<Router routes={[]}/>)
+        expect(true).toBeTruthy()
+    })
+    it("should render 404 if no routes match", ()=>{
+        render(<Router routes={[]} defaultComponent={()=><h1>404</h1>}></Router>)
+        expect(screen.getByText("404")).toBeTruthy();
+    })
+    it("should render the component of the first route that matches", ()=>{
+        const routes = [
+            {
+                path: "/",
+                Component:()=><h1>Home</h1>
+            },
+                        {
+                path: "/about",
+                Component:()=><h1>About</h1>
+            }
+        ]
+        render(<Router routes={routes}></Router>)
+        expect(screen.getByText("Home")).toBeTruthy();
     })
 })
